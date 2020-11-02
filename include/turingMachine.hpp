31b55b28aa24f44a4648e900f6b5da22b7d713eb
Sergio Guerra <alu0101133201@ull.edu.es>
2020-11-02 18:17:07 +0000

    Inicialización del directorio

---
+++ include/turingMachine.hpp
@@ -0,0 +1,11 @@
+/** 
+ * Fichero que define la clase máquina de turing
+ * ULL - Complejidad Computaciones
+ * Sergio Guerra Arencibia
+ * 02/11/2020
+ */
+
+class TuringMachine {
+  private:
+  public:
+};
